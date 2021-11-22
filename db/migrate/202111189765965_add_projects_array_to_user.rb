3e5a4c156b3f0cb0c1b9d9a0c9de2e05d3c709cf
ahmedhelaly <ahmedehabhelaly123@gmail.com>
2021-11-23 02:29:49 +0300

    Fixed migration file for deployment

--- db/migrate/202111189765965_add_projects_array_to_user.rb
+++ db/migrate/202111189765965_add_projects_array_to_user.rb
@@ -1,7 +1,8 @@
 class AddProjectsArrayToUser < ActiveRecord::Migration[6.1]
     def change
         add_column :users, :projects_id, :integer, array:true, default: []
-        remove_column :projects, :user_id
-        rename_column :users, :fullname, :username
+        remove_column :projects, :user_id , if_exists: true
+        remove_column :users, :fullname , if_exists: true
+        add_column :users, :username , :string ,if_exists: false
     end
 end
