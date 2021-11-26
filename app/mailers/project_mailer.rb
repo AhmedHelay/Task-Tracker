13a0834a1ed58fb8df2f6ef9421b5ddbcbcb3e11
ahmedhelaly <ahmedehabhelaly123@gmail.com>
2021-11-26 04:50:39 +0300

    Mailer for projects on editing

--- app/mailers/project_mailer.rb
+++ app/mailers/project_mailer.rb
@@ -5,11 +5,14 @@ class ProjectMailer < ApplicationMailer
         mail(to: @user.email, subject: 'You created project successfully')
     end
 
+    def project_edited 
+        mail(to: @user.email, subject: 'You created project successfully')
+        send_to_other_users(subject: "User #{@user.id} edited project #{@project.name}")
+    end
+
     def add_user_to_project
         mail(to: @user.email, subject: "You were added to project #{@project.name}")    
-        User.all.each do |u|
-            if u.projects_id.include?(@project.id) && u != @user
-                mail(to: u , "User #{@user.id}  was added to project #{@project.name}")
+        send_to_other_users(subject: "User #{@user.id}  was added to project #{@project.name}")
     end
 
     private
@@ -18,5 +21,11 @@ class ProjectMailer < ApplicationMailer
         @project  = params[:project]
         @user  = params[:user]
         @url  = "http://localhost:3000/projects/#{@project.id}"
+    end
+
+    def send_to_other_users
+        User.all.each do |u|
+            if u.projects_id.include?(@project.id) && u != @user
+                mail(to: u , :subject)
     end
 end
