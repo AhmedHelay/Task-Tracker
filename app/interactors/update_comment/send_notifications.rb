c09b00928346ab3ba4c3084663bf7b5d88cbe7da
AhmedEhabEl-din <ahmedehabhelaly123@gmail.com>
2021-12-03 04:12:46 +0300

    Mail features & Interacors Basis

---
+++ app/interactors/update_comment/send_notifications.rb
@@ -0,0 +1,17 @@
+class UpdateComment
+    class SendNotifications
+        include Interactor
+
+        delegate :comment, :current_user, to: :context
+
+        def call
+            create_avtivity
+        end
+
+        private 
+
+        def create_avtivity
+            RegisterActivityJob.perform_now(current_user.id, "Comment Updated", comment.id, "Comment")
+        end
+    end
+end
